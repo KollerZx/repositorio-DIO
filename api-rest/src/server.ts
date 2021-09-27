48e3329a2ad8ca36d131ab617da099999573ffa1
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2021-09-27 14:34:13 -0300

    implementado rotas e metodos para  get, post, put e delete

--- api-rest/src/server.ts
+++ api-rest/src/server.ts
@@ -1,8 +1,10 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
 import { router } from './routes'
 
 const app = express()
-
+app.use(express.json())
+app.use(urlencoded({ extended:true }))
 app.use(router)
+
 
 app.listen(3000, () => console.log(`running in http://localhost:${3000}`))
--- authentication-microservice/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response, NextFunction } from "express";
-
-const router = Router()
-
-router.get('/status', ( req: Request, res: Response, next: NextFunction ) => {
-    res.status(200).send({ foo: 'bar'})
-})
-
-export { router }
