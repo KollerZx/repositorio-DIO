519422c63e5dad4bc11be0322edf83f8d9376ecb
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2021-10-06 17:06:26 -0300

    renomeado diretorio

--- Learning-ReactJs/github-api/src/Global/globalCSS.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components'
-
-export const GlobalCSS = createGlobalStyle`
-  body{
-    font-family: 'Roboto', sans-serif;
-    font-size: 67.5%;
-  }
-
-`
