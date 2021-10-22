5730fd08e8fdda9e4b19045935bfb2e5ceea4137
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2021-10-22 16:31:14 -0300

    exemplo de render-props

---
+++ Learning-ReactJs/react-dio/render-props/App.js
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+import Counter from './Counter'
+const Buttons = ({ increment, decrement, count}) =>(
+    <div>
+        <h1>Valor atual: {count}</h1>
+        <div>
+            <button onClick={ increment }>Adicionar</button>
+        </div>
+        <div>
+            <button onClick={ decrement }>Remover</button>
+        </div>
+    </div>
+
+)
+class App extends Component {
+
+    render(){
+        return(
+            <div>
+               <Counter render={ 
+                   ({ increment, decrement, count}) => (
+                       <Buttons 
+                        increment={increment}
+                        decrement={decrement}
+                        count={count}
+                       />
+                   )   
+                }>
+
+               </Counter>
+            </div>
+        )
+    }
+}
+export default App
